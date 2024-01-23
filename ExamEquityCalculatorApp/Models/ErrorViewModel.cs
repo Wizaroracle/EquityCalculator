84425a6f5ce95e918d3ba162606098ac20ee9b40
Wizaroracle <christianjonelcananes@gmail.com>
2024-01-23 14:38:55 +0800

    Final

--- ExamEquityCalculatorApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ExamEquityCalculatorApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
